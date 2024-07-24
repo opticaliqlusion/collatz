4d382902e53da56aea9b19ad6151d7c666d05f1f
opticaliqlusion <john.laky1@gmail.com>
2024-07-23 21:38:28 -0400

    Further minimizations

--- supermin.py
+++ supermin.py
@@ -1,7 +1,6 @@
 import sympy
 BIT_LIMIT = 100000
 MAX_TESTS = 10000000
-MAX_COEFFICIENT = 50
 def _reduce(c,n):
     seen = []
     while n != 1:
@@ -12,9 +11,7 @@ def _reduce(c,n):
         if n.bit_length() > BIT_LIMIT or n in seen: break
         seen.append(n)
     return n
-def analyze_coefficient(c):
+def is_A374670(c):
     for i in range(1, MAX_TESTS):
         if _reduce(c, i) != 1: return False
     return True
-sequence = filter(lambda c:analyze_coefficient(c), range(3, MAX_COEFFICIENT, 2))
-print(list(sequence))
